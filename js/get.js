3c4c72b48de674d790184bdfd175075d9f9f5cec
engg-is-fun <shivamsharma3912@gmail.com>
2021-06-10 08:45:44 +0530

    java script file

---
+++ js/get.js
@@ -0,0 +1,23 @@
+// document.write("hello")
+var element;
+// element = document.title;
+// console.log(element);
+// element = document.body;
+// console.log(element);
+// element = document.html;
+// console.log(element);
+// element=document.getElementById("a").innerText;
+// element=document.getElementById("a").innerHTML;
+// element=document.getElementById("a").getAttribute("style");
+// element=document.getElementById("a").getAttributeNode("style");
+// element=document.getElementById("a").attributes[0];
+// console.log(element);
+
+
+
+
+
+
+////////query selector get or get dono//
+document.queryselecter("#a").innerHTML ="<center><h1>wow<h1></center>"
+document.queryselecter(".c").innerHTML ="<center><h1>taj<h1></center>"
---
+++ js/set.js
@@ -0,0 +1,3 @@
+document.getElementById("a").innerHTML ="<center><h1> Pninfosys gwlaior </h1></center>"
+document.getElementById("b").innerHTML ="<h1>phone:8462841009</h1>"
+document.getElementById("a").set.attribute("style","border:10px dotted red")
